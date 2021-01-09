5501af53dfb851550ccdf998fe6c3a1b800b1637
Hector Remedios <hectoremedio90@gmail.com>
2021-01-09 17:53:16 -0600

    heart like animation added

---
+++ src/Animation/Like/HeartLike.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+import './HeartLike.css';
+
+
+const HeartLike = () =>{
+        const [clase, setClase] = useState(true);
+        
+        const handleClick = () => {
+            setClase(!clase);
+        };
+
+    return(
+        <div className="like-box">
+            <div onClick={handleClick} className={clase ? 'like' : 'liked'}></div>
+            </div>
+    )
+}
+
+
+export default HeartLike;
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HeartLike from '../src/Animation/Like/HeartLike'
+
+function App() {
+  return (
+    <div className="App">
+      <HeartLike />
+    </div>
+  );
+}
+
+export default App;
