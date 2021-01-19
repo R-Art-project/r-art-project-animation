342a1b9de03976ceee7094c348cefa8c9e20b960
Hector Remedios <hectoremedio90@gmail.com>
2021-01-19 00:25:33 -0600

    Card Animatión 3D

--- src/Animation/Card3D/Card3D.js
+++ src/Animation/Card3D/Card3D.js
@@ -1,13 +1,18 @@
 import React from 'react'
-
+import CardFront from '../../Assets/CardFront.png'
+import Cardback from '../../Assets/CardBack.png'
 import './Card3D.css'
 
 const Card3D = () => {
     return(
         <section className='CardContainer'>
             <div className='Card'>
-                <div className='card__face card__face--front'>Front</div>
-                <div className='card__face card__face--back'>back</div>
+                <div className='card__face card__face--front'>
+                    <img src={CardFront} alt='bridge'/>
+                </div>
+                <div className='card__face card__face--back'>
+                <img src={Cardback} alt='bridge'/>
+                </div>
             </div>
         </section>
     )
